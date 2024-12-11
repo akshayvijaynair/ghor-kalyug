435d2091cd071f5da4208312bf69e65c8633bff0
akshayvijaynair <akshay.v.nair@pace.edu>
2024-12-10 19:25:27 -0500

    feat(promt): updated prompt

--- server/src/gemini.ts
+++ server/src/gemini.ts
@@ -5,8 +5,7 @@ export default function getPrompt (
     difficultyLevel: string
 ): string {
     return `Topic: ${topicString}
-Generate ${numQuestions} multiple-choice questions based on the topics above at a difficulty level of ${difficultyLevel}.
-Each question MUST have exactly 4 options. 
+Generate ${numQuestions} questions with exactly 4 options based on the topics above at a difficulty level of ${difficultyLevel}.
 Each question MUST have 1 one answer.
 Each question MUST have following format:
     Question ID: Question text?
