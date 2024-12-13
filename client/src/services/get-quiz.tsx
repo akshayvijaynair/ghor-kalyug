7f12bcb51323d96cb04181e9d08bad1b805892d5
akshayvijaynair <akshay.v.nair@pace.edu>
2024-12-13 13:48:49 -0500

    feat(Quiz): added API to retrieve quiz questions and answers

---
+++ client/src/services/get-quiz.tsx
@@ -0,0 +1,26 @@
+import { QuizResponse } from '../types/quiz.tsx';
+
+
+export const getQuiz = async (params: string): Promise<QuizResponse> => {
+    const domain = import.meta.env.VITE_API_DOMAIN;
+    console.log(domain);
+    const endpoint = `${domain}/quiz?id=${params}`;
+
+    try {
+        const response = await fetch(endpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch quiz data. Status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching quiz:', error);
+        throw error; // Allow the caller to handle the error
+    }
+};
